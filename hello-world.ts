48231ac5d6fcc58b9e48ce5d819fde13e700372c
hoyuck_j <hoyuck_j@etna-alternance.net>
2024-11-29 11:47:32 +0100

    Change greeting format

--- hello-world.ts
+++ hello-world.ts
@@ -1,8 +1,4 @@
 
-// hello-world.ts
-const greet = (name: string): string => {
-    return `Hello, ${name}!`;
-};
 
 console.log(greet("World"));// hello-world.ts
 const greet = (name: string): string => {
