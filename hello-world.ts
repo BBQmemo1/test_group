2b69bb71295a213f9d221bcad795b7b946bc302c
hoyuck_j <hoyuck_j@etna-alternance.net>
2024-11-29 11:35:29 +0100

    Add farewell function

--- hello-world.ts
+++ hello-world.ts
@@ -1,4 +1,10 @@
+
 // hello-world.ts
+const greet = (name: string): string => {
+    return `Hello, ${name}!`;
+};
+
+console.log(greet("World"));// hello-world.ts
 const greet = (name: string): string => {
     return `Hello, ${name}!`;
 };
